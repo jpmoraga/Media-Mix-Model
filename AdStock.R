c1d163e3a37ebc55c1bc27c83aa39ef20415b0e4
Juan Pablo Moraga <jp@lemonpot.com>
2018-08-22 16:58:03 -0300

    Add: Funcion función para optimizar el Adstock

--- AdStock.R
+++ AdStock.R
@@ -24,31 +24,33 @@ AdStock <- function(df,field,rate = 0.9) { #Ejemplo: AdStock(Base_OMO,35,0.7)
 media <- c(5:6) #Columnas con inversión en medios
 v <- Base_OMO$VENTA #Se guarda en v, la columna venta
 
+Opt_Adstock <- function(df,spend_field,media) { #Ejemplo Opt_Adstock(Base_OMO,39,c(5:8))
+  #df, es el data frame inicial
+  #spend_field, es el número de la columna donde está la venta
+  #media, es un vector con todos los numoers de columnas donde hay inversión en medios
 
-rates <- seq(0.001,0.999,0.001) #Creamos un vector con una secuencia de ratios, de 0.1 a 0.9 avanzando de 0.001 Para los ratios
-
-
-f <- NULL #Se crea una variable nula, si no se agrega genera error
-for(j in media) { #For para avanzar en las columnas de medios
-  c <- NULL #Se crea una variable nula, si no se agrega genera error
-  for(i in rates) { #Ciclo para probar con todos los ratios
-      x <- AdStock(Base_OMO,j,i) #Se ocupa la función Adstock para el rato y medio elegido
-      c1 <- cbind(cor(x,v),i) #Se saca la correlación entre x y v, y se ocupa cbind (column bind) para hacer un vector 1x2 con la correlación y el ratio 
-      c <- rbind(c,c1)#Agrega filas hacia abajo con la correlación y cada ratio
-      c <- as.data.frame(c) #guarda todo esto en un dataframe llamad c
-      #cmax <- c[c[,1] == max(c[,1]),] 
-      #cmin <- c[c[,1] == min(c[,1]),]
-      #c <- rbind(cmax,cmin) 
-      #c <- c[c[,1] == max(c[,1]),]
+  rates <- seq(0.001,0.999,0.001) #Creamos un vector con una secuencia de ratios, de 0.1 a 0.9 avanzando de 0.001 Para los ratios
+  
+  v <- df[,spend_field]
+  
+  fin <- NULL #Se crea una variable nula, si no se agrega genera error
+  for(j in media) { #For para avanzar en las columnas de medios
+    c <- NULL #Se crea una variable nula, si no se agrega genera error
+    for(i in rates) { #Ciclo para probar con todos los ratios
+        x <- AdStock(df,j,i) #Se ocupa la función Adstock para el rato y medio elegido
+        c1 <- cbind(cor(x,v),i) #Se saca la correlación entre x y v, y se ocupa cbind (column bind) para hacer un vector 1x2 con la correlación y el ratio 
+        c <- rbind(c,c1)#Agrega filas hacia abajo con la correlación y cada ratio
+        c <- as.data.frame(c) #guarda todo esto en un dataframe llamad c
+    }
+  colnames(c) <- c('corr','dacay') #Agregamos nombres a las columnas de df
+  c$sign <- sign(c$corr) #Agregamos una nueva columna con el "signo" de la correlación + o -
+  c$corr <- abs(c$corr) #Sacamos el valor absoluto de la correlación
+  cmax <- c[c[,1] == max(c[,1]),] #Se filtra c por el máximo
+  cmax$field <- j #Se agrega la columna con el número del campo
+  f1 <- cmax #Se renombra cmax en f1
+  fin <- rbind(fin,f1) #Los f1 se van acumulando hacia abajo en un dataframe
+  print(paste((j-min(media)+1)/length(media)*100,'%',sep = ' ')) #Se agrega porcentaje de avance
   }
-colnames(c) <- c('corr','dacay') #Agregamos nombres a las columnas de df
-c$sign <- sign(c$corr) #Agregamos una nueva columna con el "signo" de la correlación + o -
-c$corr <- abs(c$corr) #Sacamos el valor absoluto de la correlación
-cmax <- c[c[,1] == max(c[,1]),] #Se filtra c por el máximo
-cmax$field <- j #Se agrega la columna con el número del campo
-f1 <- cmax #Se renombra cmax en f1
-f <- rbind(f,f1) #Los f1 se van acumulando hacia abajo en un dataframe
-print(paste((j-min(media)+1)/length(media)*100,'%',sep = ' ')) #Se agrega porcentaje de avance
+  return(fin)
 }
-
 
